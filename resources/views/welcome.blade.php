3ceec244330d0a6d54090046361910fd065b8e8a
Eugen <64443813+ecool7@users.noreply.github.com>
2025-12-01 09:35:32 +0300

    Update welcome.blade.php
    Убрал телефон + поправил адрес

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -314,12 +314,6 @@
                                         </svg>
                                         <span>info@elementtouch.com</span>
                                     </div>
-                                    <div class="flex items-center text-white">
-                                        <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path>
-                    </svg>
-                                        <span>+1 (555) 123-4567</span>
-                                    </div>
                                 </div>
                                 <button class="mt-6 w-full bg-white text-gray-800 py-3 px-6 rounded-lg font-semibold hover:bg-gray-100 transition-colors">
                                     Request Quote
@@ -576,11 +570,6 @@
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path>
                                     </svg>
                                 </div>
-                                <div>
-                                    <h4 class="font-semibold text-gray-900 mb-1">Phone</h4>
-                                    <p class="text-gray-600">+1 (555) 123-4567</p>
-                                    <p class="text-gray-600">+1 (555) 987-6543</p>
-                                </div>
                             </div>
                             
                             <div class="flex items-start space-x-4">
@@ -592,7 +581,9 @@
                                 </div>
                                 <div>
                                     <h4 class="font-semibold text-gray-900 mb-1">Address</h4>
-                                    <p class="text-gray-600">123 Technology Drive<br>Silicon Valley, CA 94000<br>United States</p>
+                                    <p class="text-gray-600"> 715 Kangtai Group Building, <br>
+                                                            No. 222 Kefa Road, Science & Technology Park,<br>
+                                                            Nanshan District, Shenzhen</p>
                                 </div>
                             </div>
                         </div>
